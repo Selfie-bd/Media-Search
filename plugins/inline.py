9d4408d7759b35753a1c5e793f9ed11d13d788bf
CLÆY <78960870+CLaY9950@users.noreply.github.com>
2021-08-19 08:58:23 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -90,8 +90,8 @@ async def answer(bot, query):
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122')
+            InlineKeyboardButton('📡sʜᴀʀᴇ📡', url='https://t.me/share/url?url=%20https://t.me/PrimeFlix_Chats'),
+            InlineKeyboardButton('Our-LinkZ', url='https://t.me/PrimeFlixMedia_All')
         ]
         ]
     return InlineKeyboardMarkup(buttons)
