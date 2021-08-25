884f985f0273ff5a0c1134d02fed1ed0a490ec8a
CLÆY <78960870+CLaY9950@users.noreply.github.com>
2021-08-25 12:09:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -91,7 +91,7 @@ async def filter(client, message):
         buttons = data['buttons'][0].copy()
 
         buttons.append(
-            [InlineKeyboardButton(text="NEXT ⏩",callback_data=f"next_0_{keyword}")]
+            [InlineKeyboardButton(text="🚀𝗡𝗘𝗫𝗧🚀",callback_data=f"next_0_{keyword}")]
         )    
         buttons.append(
             [InlineKeyboardButton(text=f"🔖 Pages 1/{data['total']}🔖",callback_data="pages")]
