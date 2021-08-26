29ba3c3cc9fde3b42e5d8b9e8f148127887265eb
CLÆY <78960870+CLaY9950@users.noreply.github.com>
2021-08-26 18:56:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -88,10 +88,10 @@ async def filter(client, message):
             if API_KEY:
                 poster=await get_poster(search)
             if poster:
-                await message.reply_photo(photo=poster, caption=f"<b>🇵‌🇫‌🇲 ᶜʰᵃᵗˢ \n\n🎬:Movie: {up_search}\n🌩️ Total Results: {len(buttons)}\n\n© @PrimeFlixMedia_All ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_photo(photo=poster, caption=f"<b>🇵‌🇫‌🇲 ᶜʰᵃᵗˢ \n\n🎬:Movie: {up_search}\n🌩️ Total Results: {len(btns)}\n\n© @PrimeFlixMedia_All ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
 
             else:
-                await message.reply_text(f"<b>🇵‌🇫‌🇲 ᶜʰᵃᵗˢ \n\n🎬:Movie: {up_search}‌‎\n🌩️ Total Results: {len(buttons)}\n\n© @PrimeFlixMedia_All ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+                await message.reply_text(f"<b>🇵‌🇫‌🇲 ᶜʰᵃᵗˢ \n\n🎬:Movie: {up_search}‌‎\n🌩️ Total Results: {len(btns)}\n\n© @PrimeFlixMedia_All ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
             return
 
         data = BUTTONS[keyword]
@@ -107,9 +107,9 @@ async def filter(client, message):
         if API_KEY:
             poster=await get_poster(search)
         if poster:
-            await message.reply_photo(photo=poster, caption=f"<b>🇵‌🇫‌🇲 ᶜʰᵃᵗˢ \n\n🎬:Movie: {up_search}\n🌩️ Total Results: {len(buttons)}\n\n© @PrimeFlixMedia_All ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_photo(photo=poster, caption=f"<b>🇵‌🇫‌🇲 ᶜʰᵃᵗˢ \n\n🎬:Movie: {up_search}\n🌩️ Total Results: {len(btns)}\n\n© @PrimeFlixMedia_All ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
         else:
-            await message.reply_text(f"🇵‌🇫‌🇲 ᶜʰᵃᵗˢ \n\n<b>🎬:Movie: {up_search}\n🌩️ Total Results: {len(results)}\n\n© @PrimeFlixMedia_All ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
+            await message.reply_text(f"🇵‌🇫‌🇲 ᶜʰᵃᵗˢ \n\n<b>🎬:Movie: {up_search}\n🌩️ Total Results: {len(btns)}\n\n© @PrimeFlixMedia_All ‌‌‌‌‎ ­  ­  ­  ­  ­  </b>", reply_markup=InlineKeyboardMarkup(buttons))
 
 @Client.on_message(filters.text & filters.group & filters.incoming & filters.chat(AUTH_GROUPS) if AUTH_GROUPS else filters.text & filters.group & filters.incoming)
 async def group(client, message):
