148cc3e2c69b4dcd8fc257c70d971dd9300b0fb8
CLÆY <78960870+CLaY9950@users.noreply.github.com>
2021-08-29 14:30:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -209,7 +209,7 @@ async def bot_info(bot, message):
         ]
     await message.reply(text="<b>Developer : <a href='https://t.me/CLaY995'>CLAEY</a>\nLanguage : <code>Python3</code>\nLibrary : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio</a>\nSource Code : <a href='https://t.me/Oomban_ULLATH'>Click here</a>\nUpdate Channel : <a href='https://t.me/PrimeFlixMedia_All'>👉😁😁👈</a> </b>", reply_markup=InlineKeyboardMarkup(buttons), disable_web_page_preview=True)
 
-@Client.on.message(filters.command('mal_help'))
+@Client.on_message(filters.command('mal_help'))
 async def mal_help(bot, message):
     buttons = [
         [
