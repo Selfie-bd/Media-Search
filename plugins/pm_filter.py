1f994c3128229c65127113408962aa57ecf42677
CLÆY <78960870+CLaY9950@users.noreply.github.com>
2021-09-23 18:35:05 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -451,8 +451,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer("😑This is NOT for YoU❗, Search Up on ur Own",show_alert=True)
         return
 
-        elif query.data == "admn_list":
-            await query.answer()
+    elif query.data == "admn_list":
+          await query.answer()
     else:
         await query.answer("Tag any one of these ADMINS:\n\n@CLaY995\n@nakul006\n@N_i_8_m_a_r_e\n@RFt_CyberPro", show_alert=True)
   
