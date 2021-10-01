5c6a98d34456df44f6ccbbc2dd54455a57c6e532
CLÆY <78960870+CLaY9950@users.noreply.github.com>
2021-10-01 21:34:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -8,7 +8,7 @@ from pyrogram import Client, filters
 import re
 from pyrogram.errors import UserNotParticipant
 from utils import get_filter_results, get_file_details, is_subscribed, get_poster
-
+from plugins.group_filters import get_size
 
 BUTTONS = {}
 BOT = {}
